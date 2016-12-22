70ac63cbda46324f550df2755fa160181669f2b0
unsigned <konndennsa@gmail.com>
2016-12-22 16:08:16 +0900

    fix

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/vim ~/.vim
 
 # zsh
-ln -sf ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
