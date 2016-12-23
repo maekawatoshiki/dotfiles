f8d644849f12a3231435edb79b24ec8307c89203
maekawatoshiki <konndennsa@gmail.com>
2016-12-23 21:28:04 +0900

    add tmux.conf

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ git clone http://github.com/maekawatoshiki/dotfiles ~/.dotfiles
 # vim
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
