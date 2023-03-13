2e8e12ea50e08a07678793d0b1f47da9bab0c984
maekawatoshiki <konndennsa@gmail.com>
2023-03-13 19:51:57 +0900

    nodenv

---
+++ nodenv-install.sh
@@ -0,0 +1,10 @@
+# https://github.com/nodenv/nodenv#basic-github-checkout
+
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+
+# For some reason, we need this.
+git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+
+# Install yarn.
+git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
+
