4d617b2ffff5fef8336c976f07d8a58aa3b4d439
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-06-20 15:42:01 +0900

    xterm-256color

--- docker-base-ext.sh
+++ docker-base-ext.sh
@@ -4,4 +4,4 @@ export DOCKER_BUILDKIT=1
 
 TAG=base.ext
 docker build -t uint:$TAG -f Dockerfile.base.ext .
-docker run -w "/home/uint" --rm -it uint:$TAG
+docker run -e "TERM=xterm-256color" -w "/home/uint" --rm -it uint:$TAG
