ade2ecd8c619b4c73650ee1e27a98feb761431e5
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-07-15 18:32:27 +0900

    docker-base-ext.sh: Check tty

--- docker-base-ext.sh
+++ docker-base-ext.sh
@@ -3,12 +3,17 @@
 export DOCKER_BUILDKIT=1
 
 TAG=base.ext
+
 docker build -t uint:$TAG -f Dockerfile.base.ext .
-docker run \
-  --rm \
-  -it \
-  -e "TERM=xterm-256color" \
-  -w "/work" \
-  -v "$PWD:/work" \
-  -u "$(id -u):$(id -g)" \
-  uint:$TAG
+
+# Start the container if running in terminal
+if [ -t 0 ]; then
+  docker run \
+    --rm \
+    -it \
+    -e "TERM=xterm-256color" \
+    -w "/work" \
+    -v "$PWD:/work" \
+    -u "$(id -u):$(id -g)" \
+    uint:$TAG
+fi
