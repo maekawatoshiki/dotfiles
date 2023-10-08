6d7a471c96d3f92fae1c59b61e417734f908c0a9
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-10-08 23:28:06 +0900

    Add docker.cuda.sh and Dockerfile.cuda

---
+++ docker.cuda.sh
@@ -0,0 +1,19 @@
+#/bin/sh -eux
+
+export DOCKER_BUILDKIT=1
+
+TAG=cuda
+
+docker build -t uint:$TAG -f Dockerfile.cuda .
+
+# Start the container if running in terminal
+if [ -t 0 ]; then
+  docker run \
+    --rm \
+    -it \
+    -e "TERM=xterm-256color" \
+    -w "/work" \
+    -v "$PWD:/work" \
+    -u "$(id -u):$(id -g)" \
+    uint:$TAG
+fi
