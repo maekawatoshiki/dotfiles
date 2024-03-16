eea15677099ed212d0098c7433400a36d468520c
maekawatoshiki <maekawatoshiki1017@gmail.com>
2024-03-16 17:01:40 +0900

    docker.sh: Auto-rebuild image

--- docker.sh
+++ docker.sh
@@ -2,9 +2,16 @@
 
 export DOCKER_BUILDKIT=1
 
+NAME=uint
 TAG=base.ext
 
-docker build -t uint:$TAG -f Dockerfile .
+IMG_UPDATE_TIME=$(docker inspect --format '{{ .Created }}' ${NAME}:${TAG} | xargs date +%s -d)
+DOCKERFILE_UPDATE_TIME=$(date +%s -r Dockerfile)
+
+# Rebuild the image if Dockerfile is updated
+if [ $DOCKERFILE_UPDATE_TIME -gt $IMG_UPDATE_TIME ]; then
+  docker build -t $NAME:$TAG -f Dockerfile .
+fi
 
 # Start the container if running in terminal
 if [ -t 0 ]; then
@@ -15,5 +22,5 @@ if [ -t 0 ]; then
     -w "/work" \
     -v "$PWD:/work" \
     -u "$(id -u):$(id -g)" \
-    uint:$TAG
+    $NAME:$TAG
 fi
