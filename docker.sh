6c26f001f15d24045e007f01c8722d2aa3b6f133
maekawatoshiki <maekawatoshiki1017@gmail.com>
2024-04-02 02:20:33 +0900

    Add docker.sh

--- docker.sh
+++ docker.sh
@@ -8,7 +8,7 @@ USER=uint
 
 IMG_UPDATE_TIME=0
 # Check if the image exists
-if [ $(docker image inspect ${imageName}:${imageTag} > /dev/null 2>&1) ]; then
+if docker image inspect ${NAME}:${TAG} > /dev/null 2>&1; then
   # Get the image creation time
   IMG_UPDATE_TIME=$(docker inspect --format '{{ .Created }}' ${NAME}:${TAG} | xargs date +%s -d)
 fi
