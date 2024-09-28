8e2921a0f0d1440eea1e0db148deca26fd9c3018
maekawatoshiki <maekawatoshiki1017@gmail.com>
2024-09-28 22:39:21 +0900

    ssh/config

--- setup.sh
+++ setup.sh
@@ -64,6 +64,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 cp ./zshrc ~/.zshrc
 cp ./p10k.zsh ~/.p10k.zsh
 
+mkdir ~/.ssh
+cp ./ssh_config ~/.ssh/config
+
 # git
 ln -s $PWD/gitconfig $HOME/.gitconfig
 
