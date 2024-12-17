49980bbe5626318248d8e92f9232a084c7a6756a
maekawatoshiki <maekawatoshiki1017@gmail.com>
2024-12-18 08:20:06 +0900

    Fedora env

---
+++ docker.fedora.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -eux
+
+export DOCKER_BUILDKIT=1
+
+NAME=fedora-test
+TAG=latest
+USER=uint
+
+IMG_UPDATE_TIME=0
+# Check if the image exists
+if docker image inspect ${NAME}:${TAG} > /dev/null 2>&1; then
+  # Get the image creation time
+  IMG_UPDATE_TIME=$(docker inspect --format '{{ .Created }}' ${NAME}:${TAG} | xargs date +%s -d)
+fi
+
+DOCKERFILE_UPDATE_TIME=$(date +%s -r Dockerfile.fedora)
+
+# Rebuild the image if Dockerfile is updated
+if [ $DOCKERFILE_UPDATE_TIME -gt $IMG_UPDATE_TIME ]; then
+  docker build \
+    -t $NAME:$TAG \
+    --build-arg USER=$USER \
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    -f Dockerfile.fedora .
+fi
+
+# Start the container if running in terminal
+if [ -t 0 ]; then
+  docker run \
+    --rm \
+    -it \
+    --privileged \
+    -e "TERM=xterm-256color" \
+    -w "/work" \
+    -v "$PWD:/work" \
+    $NAME:$TAG
+fi
+
---
+++ setup-fedora.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -eux
+
+sudo dnf update -y
+sudo dnf install -y \
+    @development-tools \
+    bash-completion \
+    make \
+    automake \
+    gcc \
+    g++ \
+    kernel-devel \
+    curl \
+    git \
+    gnupg2 \
+    iputils \
+    jq \
+    less \
+    net-tools \
+    openssh-clients \
+    tar \
+    time \
+    unzip \
+    vim \
+    xz \
+    zip \
+    clang \
+    llvm \
+    lld \
+    ccache \
+    ninja-build \
+    cmake \
+    gettext \
+    libtool \
+    autoconf \
+    pkgconfig \
+    doxygen \
+    zsh \
+    python3 \
+    python3-pip \
+    python3-virtualenv \
+    wget \
+    git-lfs \
+    htop \
+    zlib-devel \
+    openssh-server \
+    rsync \
+    libevent-devel \
+    bison \
+    ncurses-devel \
+    gettext-devel \
+    neovim
+
+# zsh
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp ./zshrc ~/.zshrc
+cp ./p10k.zsh ~/.p10k.zsh
+
+mkdir -p ~/.ssh
+cp ./ssh_config ~/.ssh/config
+
+# git
+ln -s $PWD/git/gitconfig $HOME/.gitconfig
+
+# Build neovim
+
+mkdir -p ~/work
+
+# (
+#   cd ~/work
+#   git clone https://github.com/neovim/neovim --depth 1 --recursive \
+#       && cd neovim \
+#       && CC='clang' CXX='clang++' \
+#           make -j CMAKE_BUILD_TYPE=Release \
+#       && sudo make -j CMAKE_BUILD_TYPE=Release install
+# )
+
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/nvim/lua
+ln -s $(pwd)/vim/plugins.lua $HOME/.config/nvim/lua/plugins.lua
+ln -s $(pwd)/vim/init.lua $HOME/.config/nvim/init.lua
+
