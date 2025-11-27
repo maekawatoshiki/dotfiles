f9b010afbb918d0c6fb6e889823f2acbb838d0a5
Toshiki Maekawa <maekawatoshiki1017@gmail.com>
2025-11-28 01:35:46 +0900

    nix: Add packages

--- nix/flake.nix
+++ nix/flake.nix
@@ -18,9 +18,64 @@
       name = "uint-packages";
       paths = [
         pkgs.git
+        pkgs.git-lfs
+
         pkgs.curl
+        pkgs.wget
+
         pkgs.neovim
+
         pkgs.clang
+        pkgs.clang-tools
+        pkgs.lld
+        pkgs.ccache
+        pkgs.cmake
+        pkgs.ninja
+        pkgs.pkg-config
+        pkgs.boost.dev
+
+        pkgs.qemu
+
+        pkgs.go
+        pkgs.nodenv
+        pkgs.pyenv
+
+        pkgs.zsh
+        pkgs.tmux
+        pkgs.fzf
+        pkgs.btop
+        pkgs.htop
+        pkgs.shellcheck
+
+        pkgs.coreutils
+        pkgs.findutils
+        pkgs.tree
+        pkgs.jq
+        pkgs.watch
+        pkgs.pv
+
+        pkgs.ffmpeg
+        pkgs.imagemagick
+        pkgs.xz
+        pkgs.xz.dev
+        pkgs.zlib
+        pkgs.zlib.dev
+        pkgs.libiconv
+        pkgs.gnutls
+        pkgs.llvmPackages_21.openmp
+        pkgs.mimalloc
+        pkgs.protobuf
+        pkgs.readline
+        pkgs.libffi
+        pkgs.libuuid
+        pkgs.openssl
+        pkgs.sqlite
+        pkgs.bzip2
+        pkgs.tk
+        pkgs.tcl
+        pkgs.openssl
+
+        pkgs.slackdump
       ];
     };
 
